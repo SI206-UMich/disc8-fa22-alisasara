77f1d7946abafaba319f6513a5b847f9d0ee1bd3
Alisa Rubinstein <alisarubinstein@Alisas-MacBook-Air.local>
2022-10-25 16:36:50 -0400

    first commit

--- discussion_8.py
+++ discussion_8.py
@@ -21,6 +21,9 @@ def main():
     # Task 1: Create a BeautifulSoup object and name it soup. Refer to discussion slides or lecture slides to complete this
 
     #### YOUR CODE HERE####
+    url = "https://en.wikipedia.org/wiki/University_of_Michigan"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
 
     #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
     getLink(soup)
